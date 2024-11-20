65c054c96c929238a02076b4443e7e77329f5455
doodlum <15017472+doodlum@users.noreply.github.com>
2024-11-20 16:44:09 +0000

    fix(GOG): fix crash with upscaling (#764)

--- src/Upscaling.h
+++ src/Upscaling.h
@@ -114,7 +114,9 @@ public:
 
 	static void InstallHooks()
 	{
-		stl::write_thunk_call<Main_UpdateJitter>(REL::RelocationID(75460, 77245).address() + REL::Relocate(0xE5, 0xE2, 0x104));
+		bool isGOG = !std::filesystem::exists(L"steam_api64.dll");
+
+		stl::write_thunk_call<Main_UpdateJitter>(REL::RelocationID(75460, 77245).address() + REL::Relocate(0xE5, isGOG ? 0x133 : 0xE2, 0x104));
 		stl::write_thunk_call<TAA_BeginTechnique>(REL::RelocationID(100540, 107270).address() + REL::Relocate(0x3E9, 0x3EA, 0x448));
 		stl::write_thunk_call<TAA_EndTechnique>(REL::RelocationID(100540, 107270).address() + REL::Relocate(0x3F3, 0x3F4, 0x452));
 		logger::info("[Upscaling] Installed hooks");
